dd087aaa4d950695a2d1f1f297b5b933e9c88a3b
wenjianzhang <zwj777@live.com>
2016-09-19 17:50:43 +0800

    初始化redis基本使用demo

--- RedisDemo/Program.cs
+++ RedisDemo/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StackExchange.Redis;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,22 @@ using System.Threading.Tasks;
 
 namespace RedisDemo
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            ConfigurationOptions option = new ConfigurationOptions();
+            option.EndPoints.Add("192.168.16.249", 6379);
+            option.Password = "wochu123";
+
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(option);
+            IDatabase db = redis.GetDatabase();
+            string value = "abcdefg";
+            db.StringSet("mykey", value);
+
+            Console.WriteLine(db.StringGet("mykey"));
+
+            Console.ReadLine();
         }
     }
 }
