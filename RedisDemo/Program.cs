3dadd88ad342fddb80f7860b0a68318d8133d2b3
wenjianzhang <zwj777@live.com>
2016-09-19 18:46:20 +0800

    完善

--- RedisDemo/Program.cs
+++ RedisDemo/Program.cs
@@ -11,18 +11,63 @@ namespace RedisDemo
     {
         private static void Main(string[] args)
         {
-            ConfigurationOptions option = new ConfigurationOptions();
-            option.EndPoints.Add("192.168.16.249", 6379);
-            option.Password = "wochu123";
-
-            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(option);
-            IDatabase db = redis.GetDatabase();
-            string value = "abcdefg";
-            db.StringSet("mykey", value);
-
-            Console.WriteLine(db.StringGet("mykey"));
+            CacheHelper cachehelper = new CacheHelper();
+            cachehelper.Get("123");
 
             Console.ReadLine();
+        }
+    }
+
+    public class CacheHelper
+    {
+        private ConfigurationOptions option = new ConfigurationOptions { EndPoints = { { "192.168.16.249", 6379 } }, Password = "wochu123" };
+
+        /// <summary>
+        /// 获取
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public object Get(string key)
+        {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(option);
+            IDatabase db = redis.GetDatabase();
+
+            if (db.StringGet(key).IsNullOrEmpty)
+            {
+                bool bl = db.StringSet(key, "");
+            }
+            object str = db.StringGet(key).IsNullOrEmpty;
+            return str;
+        }
+
+        /// <summary>
+        /// 设置
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool Set(string key, string value)
+        {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(option);
+            IDatabase db = redis.GetDatabase();
+            bool bl = db.StringSet(key, value);
+            return bl;
+        }
+
+        /// <summary>
+        /// 添加队列
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public string QueueAdd(string key, string value)
+        {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(option);
+            IDatabase db = redis.GetDatabase();
+            string str = db.StringGet(key);
+            db.StringSet(key, value);
+
+            return str;
         }
     }
 }
